1141003ad14971c0cf5d239c820da8632f42ba9b
MasterCard <79383995+MASTERCAR-B@users.noreply.github.com>
2024-10-14 03:40:37 -0300

    Update Filament_Fans.cpp

--- Main/Filament_Fans.cpp
+++ Main/Filament_Fans.cpp
@@ -2,17 +2,12 @@ const int ventiladorPin = 14; // Pin digital donde se conecta el ventilador
 
 void setup() {
   pinMode(ventiladorPin, OUTPUT); // Configura el pin del ventilador como salida
+  digitalWrite(ventiladorPin, HIGH); // Enciende el ventilador al inicio
   Serial.begin(9600); // Iniciar la comunicación serie para monitoreo
 }
 
 void loop() {
-  // Encender el ventilador
-  digitalWrite(ventiladorPin, HIGH); // Activa el ventilador
+  // Mantiene el ventilador encendido
   Serial.println("Ventilador encendido");
-  delay(5000); // Mantener el ventilador encendido por 5 segundos
-
-  // Apagar el ventilador
-  digitalWrite(ventiladorPin, LOW); // Desactiva el ventilador
-  Serial.println("Ventilador apagado");
-  delay(5000); // Mantener el ventilador apagado por 5 segundos
+  delay(10000); // Espera 10 segundos antes de enviar el siguiente mensaje
 }
