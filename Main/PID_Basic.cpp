a2f4ae2b99be6ece012b4458d18efb4cd15cb0f9
MasterCard <79383995+MASTERCAR-B@users.noreply.github.com>
2024-12-27 06:00:03 -0300

    Update PID_Basic.cpp

--- Main/PID_Basic.cpp
+++ Main/PID_Basic.cpp
@@ -1,35 +1,60 @@
-#include <PID_v1.h> // Include the PID library
+//from https://gist.github.com/careyi3/02a57dfd3a62a96d46171489c83488bd
 
-// PID control variables
-double Setpoint, Input, Output;
-double Kp = 2.0, Ki = 5.0, Kd = 1.0; // PID tuning parameters
+const int INPUT_PIN = A0;
+const int OUTPUT_PIN = DD3;
 
-// PID object
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+double dt, last_time;
+double integral, previous, output = 0;
+double kp, ki, kd;
+double setpoint = 75.00;
 
-void setupPID() {
-  // Initialize PID
-  Setpoint = setpointTemperature; // Initial setpoint
-  myPID.SetMode(AUTOMATIC);
-  myPID.SetOutputLimits(0, windowSize); // Output limits correspond to window size
-  myPID.SetSampleTime(1000); // Sample time in milliseconds
+void setup()
+{
+  kp = 0.8;
+  ki = 0.20;
+  kd = 0.001;
+  last_time = 0;
+  Serial.begin(9600);
+  analogWrite(OUTPUT_PIN, 0);
+  for(int i = 0; i < 50; i++)
+  {
+    Serial.print(setpoint);
+    Serial.print(",");
+    Serial.println(0);
+    delay(100);
+  }
+  delay(100);
 }
 
-void loopPID() {
-  // Compute PID output
-  myPID.Compute();
+void loop()
+{
+  double now = millis();
+  dt = (now - last_time)/1000.00;
+  last_time = now;
 
-  // Time-proportional control logic for SSR (or LED)
-  unsigned long now = millis();
-  if (now - windowStartTime > windowSize) {
-    // Time to shift the Relay Window
-    windowStartTime += windowSize;
-  }
+  double actual = map(analogRead(INPUT_PIN), 0, 1024, 0, 255);
+  double error = setpoint - actual;
+  output = pid(error);
 
-  // Control the SSR (or LED) based on the PID output
-  if (Output > now - windowStartTime) {
-    digitalWrite(ssrPin, HIGH); // SSR (or LED) ON
-  } else {
-    digitalWrite(ssrPin, LOW); // SSR (or LED) OFF
-  }
+  analogWrite(OUTPUT_PIN, output);
+
+  // Setpoint VS Actual
+  Serial.print(setpoint);
+  Serial.print(",");
+  Serial.println(actual);
+
+  // Error
+  //Serial.println(error);
+
+  delay(300);
+}
+
+double pid(double error)
+{
+  double proportional = error;
+  integral += error * dt;
+  double derivative = (error - previous) / dt;
+  previous = error;
+  double output = (kp * proportional) + (ki * integral) + (kd * derivative);
+  return output;
 }
