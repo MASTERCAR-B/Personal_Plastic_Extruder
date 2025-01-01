6308f91e809a546b2397e9ff370a65a4cfd2d85b
MasterCard <79383995+MASTERCAR-B@users.noreply.github.com>
2025-01-01 06:24:06 -0300

    Update Thermistor.cpp

--- Main/Thermistor.cpp
+++ Main/Thermistor.cpp
@@ -6,21 +6,18 @@ const float resistenciaNominal = 100000; // 100k ohm a 25 grados Celsius
 const float resistorFijo = 4700; // 4700 ohmios
 const float B = 3950; // Constante B del termistor
 const float A = 0.176323; // Constante B del termistor
-
+const float offset = 47.0;
 void setup() {
   Serial.begin(9600); // Iniciar comunicación serie
 }
 
 void loop() {
-  int reading = analogRead(termistorPin); // Leer valor analógico
-  //Serial.print(reading);
-  float voltaje = reading * (5.0 / 1023.0); // Convertir a voltaje
-  //float resistenciaTermistor = (resistorFijo * (5.0 - voltaje)) / voltaje; // Calcular resistencia del termistor
+  leer_temperatura(offset);
+}
 
-  float temperatura = -223.15+(B/log((voltaje*resistorFijo)/(A*(5.0-voltaje))));
- 
-  Serial.print("Temperatura: ");
-  Serial.print(temperatura);
-  Serial.println(" °C");
-  Serial.println(voltaje);
-  delay(1000);}
+float leer_temperatura(float offset) {
+  int reading = analogRead(termistorPin);
+  float voltaje = reading * (5.0 / 1023.0);
+  float temperatura = offset + (B / log((voltaje * resistorFijo) / (A * (5.0 - voltaje))));
+  return temperatura; 
+}
